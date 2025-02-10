a55b5166959a92bcda6a5ab7d98acd53a2c031c9
Leandro Silva <leandro94.a.s@gmail.com>
2025-02-09 22:04:07 -0300

    feat: get book by id

--- app/src/BookWise.Catalog.Application/Handlers/v1/GetBookById/GetBookByIdHandler.cs
+++ app/src/BookWise.Catalog.Application/Handlers/v1/GetBookById/GetBookByIdHandler.cs
@@ -1,11 +1,73 @@
+using System.Text.Json;
+using AutoMapper;
+using BookWise.Catalog.Application.Exceptions;
+using BookWise.Catalog.Application.Handlers.v1.CreateBook;
+using BookWise.Catalog.Application.Helpers;
+using BookWise.Catalog.Domain.Repositories;
+using BookWise.Catalog.Infrastructure.LogAudit.Abstractions;
+using BookWise.Catalog.Infrastructure.LogAudit.Dtos;
+using BookWise.Catalog.Infrastructure.LogAudit.Enums;
+using BookWise.Catalog.Infrastructure.Notifications.Abstraction;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace BookWise.Catalog.Application.Handlers.v1.GetBookById;
 
 public sealed class GetBookByIdHandler : IRequestHandler<GetBookByIdCommand, GetBookByIdResult>
 {
-    public Task<GetBookByIdResult> Handle(GetBookByIdCommand request, CancellationToken cancellationToken)
+    private readonly ILogger<CreateBookHandler> _logger;
+    private readonly INotificationService _notificationService;
+    private readonly IMapper _mapper;
+    private readonly ILogAuditService _logAuditService;
+    private readonly IBookRepository _bookRepository;
+    
+    public GetBookByIdHandler(
+        ILogger<CreateBookHandler> logger, 
+        INotificationService notificationService, 
+        IMapper mapper, 
+        ILogAuditService logAuditService, 
+        IBookRepository bookRepository)
     {
-        throw new NotImplementedException();
+        _logger = logger;
+        _notificationService = notificationService;
+        _mapper = mapper;
+        _logAuditService = logAuditService;
+        _bookRepository = bookRepository;
+    }
+    
+    public async Task<GetBookByIdResult> Handle(GetBookByIdCommand request, CancellationToken cancellationToken)
+    {
+        _ = AuditLog(request, cancellationToken);
+
+        return await GetBookByIdAsync(request, cancellationToken);
+    }
+
+    private async Task<GetBookByIdResult> GetBookByIdAsync(GetBookByIdCommand request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var book = await _bookRepository.GetByIdAsync(request.Id, cancellationToken);
+            
+            NotFoundException.ThrowIfNull(book, "Livro não cadastrado");
+            
+            return _mapper.Map<GetBookByIdResult>(book);
+        }
+        catch (Exception ex)
+        {
+            var msg = "Erro indefinido no cadastro de livro";
+            NotificationHelper.Notificar(ex, msg, _notificationService, _logger);
+            throw new InternalServerErrorException(msg);
+        }
+    }
+    
+    private Task AuditLog(GetBookByIdCommand request, CancellationToken cancellationToken)
+    {
+        var log = new LogAuditCommand(
+            operacao: AuditoriaOperacao.Insercao,
+            descricao:$"Criacao de livro" + $"request {JsonSerializer.Serialize(request) }"
+        );
+
+        return _logAuditService.AuditAsync(log);
     }
 }
--- app/src/BookWise.Catalog.Application/Mappers/BookProfile.cs
+++ app/src/BookWise.Catalog.Application/Mappers/BookProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BookWise.Catalog.Application.Handlers.v1.CreateBook;
 using BookWise.Catalog.Application.Handlers.v1.DeleteBook;
+using BookWise.Catalog.Application.Handlers.v1.GetBookById;
 using BookWise.Catalog.Application.Handlers.v1.UpdateBookCover;
 using BookWise.Catalog.Domain.Entities;
 
@@ -36,6 +37,17 @@ public class BookProfile : Profile
         
         _ = CreateMap<Book, DeleteBookResult>()
             .ForMember(dest => dest.Deleted, opt => opt.MapFrom(src => true));
-
+        
+        _ = CreateMap<Book, GetBookByIdResult>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Isbn, opt => opt.MapFrom(src => src.Isbn))
+            .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Publisher))
+            .ForMember(dest => dest.GenreBook, opt => opt.MapFrom(src => src.GenreBook))
+            .ForMember(dest => dest.NumberOfPages, opt => opt.MapFrom(src => src.NumberOfPages))
+            .ForMember(dest => dest.YearOfPublish, opt => opt.MapFrom(src => src.YearOfPublish))
+            .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate));
+            
     }
 }
